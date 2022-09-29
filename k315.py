86ba8463ea67d0f4239f7c9196ea58913f8d7438
Itshat36 <112979025+Itshat36@users.noreply.github.com>
2022-09-29 19:18:12 +0000

    update

---
+++ k315.py
@@ -0,0 +1,112 @@
+from stanfordkarel import *
+from time import sleep
+
+class ktools:
+ def m(self):
+    """shorthand for move"""
+    move()
+ def tl(self):
+   """"Turn Left"""
+   turn_left()
+ def tr(self):
+   """"Turn Right"""
+   self.tl()
+   self.tl()
+   self.tl()
+ def ta(self): 
+   """turn around"""
+   self.tl()
+   self.tl()
+ def pick(self):
+   """pick beeper"""
+   pick_beeper()
+ def put(self):
+    """Put Beeper"""
+    put_beeper()
+ def put2(self):
+    """put 2 beepers in a line"""
+    self.put()
+    self.m()
+    self.put()
+ def put5():
+   """fibe beepers in a line"""
+   self.put2()
+   self.move()
+   self.put2()
+   self.move()
+   self.put()
+ def h(self):
+   """print H"""
+   self.tl()
+   self.put5()
+   self.tr()
+   self.put5()
+   self.m()
+   self.m()
+   self.tl()
+   self.put5()
+   self.m()
+   self.m()
+   self.tl()
+   self.m()
+   self.put2()
+   self.m()
+   self.m()
+   self.tl()
+   self.m()
+   self.m()
+   self.m()
+   self.m()
+
+
+ def fourb(self):
+   self.put()
+   self.tl()
+   self.m()
+   self.m()
+   self.m()
+   self.tr()
+   self.m()
+   self.tr()
+   self.m()
+   self.m()
+   self.m()
+ def rugw(self):
+   self.m()
+   self.m()
+   self.m()
+   self.m()
+   self.m()
+   self.m()
+   self.tl()
+   self.put()
+ def rug(self):
+   self.m()
+   self.m()
+   self.m()
+   self.m()
+   self.m()
+   self.m()
+   self.m()
+   self.tl()
+   self.put()
+
+
+   
+def main():
+   """Karel code goes here!"""
+   kt = ktools()
+   kt.m()
+   kt.tl()
+   kt.m()
+   kt.tr()
+   kt.m()
+   kt.rugw()
+   kt.rug()
+   kt.rugw()
+   kt.rug()
+  
+   
+   pass
+if __name__ == "__main__":
+  run_karel_program()
